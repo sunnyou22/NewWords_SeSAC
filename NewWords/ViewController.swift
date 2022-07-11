94603797b6a8401c4aaaa64042bbd744fa8153e4
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-11 21:44:14 +0900

    Fix 1. 신조어 딕셔너리 추가 2. 신조어 검색함수 추가 3. 키보드 ReturnKey 검색기능 및 편집상태종료 액션 추가

--- NewWords/ViewController.swift
+++ NewWords/ViewController.swift
@@ -11,17 +11,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet var hashTagBtns: [UIButton]!
     @IBOutlet weak var descriptionNewWords: UILabel!
-    
     @IBOutlet weak var test: UIButton!
+    
+    var newWordList: Dictionary<String, String> = ["삼귀자": "연애를 시작하기 전 썸 단계"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         textField.layer.borderWidth = 3
         textField.layer.borderColor = UIColor.black.cgColor
         textField.keyboardType = .default
         
         makeBtnUI(hashTagBtns)
       
+    }
+    
+    func searchNewWords(_ words: UITextField) {
+        for i in newWordList {
+            if i.key == words.text {
+                descriptionNewWords.text = i.value
+            } else {
+                descriptionNewWords.text = "시스템 오류입니다."
+            }
+        }
     }
     
     func makeBtnUI(_ button : [UIButton]!) {
@@ -31,15 +43,21 @@ class ViewController: UIViewController {
             button.layer.borderWidth = 1
         }
     }
-
-    @IBAction func downKeydoard(_ sender: UITapGestureRecognizer) {
+    
+    @IBAction func downkeyboard(_ sender: Any) {
         view.endEditing(true)
     }
-    @IBAction func searchBtn(_ sender: Any) {
-        if textField.text == "삼귀자" {
-            descriptionNewWords.text = "연애를 시작하기 전 단계"
-        }
+    
+ // 아래 둘은 들어간 함수는 같은데 이벤트가 다르다. 어떻게 통일시키지..
+    @IBAction func searchWords(_ sender: UIButton) {
+        searchNewWords(textField)
+        view.endEditing(true)
     }
     
+    @IBAction func enterExitKeyboard(_ sender: UITextField) {
+        searchNewWords(textField)
+        textField.endEditing(true)
+    }
+    
+    
 }
-
