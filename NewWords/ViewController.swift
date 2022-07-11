8744aa39223ad9bd0852992e3ba8b5a3fe578ea7
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-11 17:15:07 +0900

    신조어 과제

--- NewWords/ViewController.swift
+++ NewWords/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet var hashTagBtns: [UIButton]!
+    @IBOutlet weak var descriptionNewWords: UILabel!
+    
+    @IBOutlet weak var test: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.layer.borderWidth = 3
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.keyboardType = .default
+        
+        makeBtnUI(hashTagBtns)
+      
+    }
+    
+    func makeBtnUI(_ button : [UIButton]!) {
+        for button in hashTagBtns {
+            button.layer.cornerRadius = 13
+            button.layer.borderColor = UIColor.black.cgColor
+            button.layer.borderWidth = 1
+        }
     }
 
-
+    @IBAction func downKeydoard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @IBAction func searchBtn(_ sender: Any) {
+        if textField.text == "삼귀자" {
+            descriptionNewWords.text = "연애를 시작하기 전 단계"
+        }
+    }
+    
 }
 
